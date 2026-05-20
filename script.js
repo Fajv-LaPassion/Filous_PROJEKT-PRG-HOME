a0f7c38204f2d842279f0f9b13c7e89b49f6ef7f
Fajv-Lapassion <ondrej.filous@gmail.com>
2026-05-20 19:57:10 +0200

    Přidání (thow new errow když hodnoty nebudou větší než ) Přidání class Running

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ class Workout {
     duration;
     Intensity;
     constructor(duration, Intensity) {
+        if (duration <= 0 || Intensity <= 0) {
+            throw new Error("Hodnoty musí být větší než 0!!");
+        }
         this.duration = duration;
         this.Intensity = Intensity;
+    }
+}
+class Running extends Workout {
+    distance;
+    constructor(duration, Intensity, distance) {
+        super(duration, Intensity);
+        if (distance <= 0) {
+            throw new Error("Hodnota musí být větší než 0!!");
+        }
+        this.distance = distance;
+    }
+    calculateCaloriesBurned() {
+        return this.duration * this.Intensity * this.distance * 0.1;
+    }
+    getsummary() {
+        return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km, Spálené kalorie: ${this.calculateCaloriesBurned()} kcal.`;
     }
 }
--- script.ts
+++ script.ts
@@ -1,10 +1,31 @@
 abstract class Workout {
     duration:number;
-    Intensity:string;¨
+    Intensity:number;
     constructor(duration:number, Intensity:number){
+        if(duration <= 0|| Intensity <= 0){
+            throw new Error("Hodnoty musí být větší než 0!!");
+        }
         this.duration = duration;
         this.Intensity = Intensity;
     }
     abstract calculateCaloriesBurned():number;
     abstract getsummary():string;
 }
+
+class Running extends Workout {
+    distance:number;
+    constructor(duration:number, Intensity:number, distance:number){
+        super(duration, Intensity);
+        if (distance <= 0) {
+            throw new Error("Hodnota musí být větší než 0!!");
+        }
+        this.distance = distance;
+    }
+    calculateCaloriesBurned(): number {
+        return this.duration * this.Intensity * this.distance * 0.1;
+    }
+    getsummary(): string {
+        return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km, Spálené kalorie: ${this.calculateCaloriesBurned()} kcal.`;
+    }
+}
+
