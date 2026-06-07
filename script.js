68584307a28798fbbbde6804f323f864e94dbb54
Fajv-Lapassion <ondrej.filous@gmail.com>
2026-06-07 20:34:58 +0200

    Přidání linku na framework W3.CSS, záhlaví s názvem aplikace a tabulky pro zobrazení dat. přidání funkce renderResult() do souboru script.ts, která vykresluje data do tabulky rozšíření třidy Workout od id řádku, který se plní při načítání dat ze souiboru data.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 import { workoutCatalog } from "./data.js";
 class Workout {
+    id;
     duration;
     Intensity;
-    constructor(duration, Intensity) {
+    constructor(id, duration, Intensity) {
         if (duration < 0 || Intensity < 0) {
             throw new Error("Hodnoty musí být větší než 0!!");
         }
+        this.id = id;
         this.duration = duration;
         this.Intensity = Intensity;
+    }
+    render() {
     }
 }
 class Running extends Workout {
     distance;
-    constructor(duration, Intensity, distance) {
-        super(duration, Intensity);
+    constructor(id, duration, Intensity, distance) {
+        super(id, duration, Intensity);
         if (distance < 0) {
             throw new Error("Hodnota musí být větší než 0!!");
         }
@@ -23,14 +27,14 @@ class Running extends Workout {
         return this.duration * this.Intensity * this.distance * 0.1;
     }
     getsummary() {
-        return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km, Spálené kalorie: ${this.calculateCaloriesBurned()} kcal.`;
+        return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km`;
     }
 }
 class StrengthTraining extends Workout {
     sets;
     reps;
-    constructor(duration, Intensity, sets, reps) {
-        super(duration, Intensity);
+    constructor(id, duration, Intensity, sets, reps) {
+        super(id, duration, Intensity);
         if (sets < 0 || reps < 0) {
             throw new Error("Hodnota musí být větší než 0!!");
         }
@@ -41,20 +45,36 @@ class StrengthTraining extends Workout {
         return this.duration * this.Intensity * this.sets * this.reps * 0.05;
     }
     getsummary() {
-        return `Posilování: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Série: ${this.sets}, Opakování: ${this.reps}, Spálené kalorie: ${this.calculateCaloriesBurned()} kcal.`;
+        return `Posilování: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Série: ${this.sets}, Opakování: ${this.reps}`;
     }
 }
 const myWorkouts = [];
+let itemId = 1;
 workoutCatalog.forEach(item => {
     if (item.type === 'running') {
-        myWorkouts.push(new Running(item.duration, item.Intensity, item.distance));
+        myWorkouts.push(new Running(itemId++, item.duration, item.Intensity, item.distance));
     }
     else if (item.type === 'strength') {
-        myWorkouts.push(new StrengthTraining(item.duration, item.Intensity, item.sets, item.reps));
+        myWorkouts.push(new StrengthTraining(itemId++, item.duration, item.Intensity, item.sets, item.reps));
     }
 });
-console.log("Souhrn tréninků:");
-myWorkouts.forEach(workout => {
-    console.log(workout.getsummary());
-    console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
-});
+// console.log("Souhrn tréninků:");
+// console.log(workout.getsummary());
+// console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
+export function renderResult() {
+    var tbl = document.getElementById("myTable");
+    // ---
+    myWorkouts.forEach(workout => {
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        td1.appendChild(document.createTextNode(workout.getsummary()));
+        tr.appendChild(td1);
+        var td2 = document.createElement('td');
+        td2.setAttribute('class', 'w3-right-align');
+        td2.appendChild(document.createTextNode(workout.calculateCaloriesBurned().toString() + ' kcal'));
+        tr.appendChild(td2);
+        if (tbl)
+            tbl.appendChild(tr);
+    });
+}
+window.renderResult = renderResult;
--- script.ts
+++ script.ts
@@ -1,23 +1,28 @@
-import { workoutCatalog } from "./data";
+import { workoutCatalog } from "./data.js";
 
 abstract class Workout {
+    id:number;
     duration:number;
     Intensity:number;
-    constructor(duration:number, Intensity:number){
+    constructor(id:number, duration:number, Intensity:number) {
         if(duration < 0|| Intensity < 0){
             throw new Error("Hodnoty musí být větší než 0!!");
         }
+        this.id = id;
         this.duration = duration;
         this.Intensity = Intensity;
     }
     abstract calculateCaloriesBurned():number;
     abstract getsummary():string;
+    public render():void {
+
+    }
 }
 
 class Running extends Workout {
     distance:number;
-    constructor(duration:number, Intensity:number, distance:number){
-        super(duration, Intensity);
+    constructor(id:number, duration:number, Intensity:number, distance:number) {
+        super(id, duration, Intensity);
         if (distance < 0) {
             throw new Error("Hodnota musí být větší než 0!!");
         }
@@ -27,7 +32,7 @@ class Running extends Workout {
         return this.duration * this.Intensity * this.distance * 0.1;
     }
     getsummary(): string {
-        return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km, Spálené kalorie: ${this.calculateCaloriesBurned()} kcal.`;
+        return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km`;
     }
 }
 
@@ -35,8 +40,8 @@ class StrengthTraining extends Workout {
     sets:number;
     reps:number;
 
-    constructor(duration:number, Intensity:number, sets:number, reps:number){
-        super(duration, Intensity);
+    constructor(id:number, duration:number, Intensity:number, sets:number, reps:number) {
+        super(id, duration, Intensity);
         if (sets < 0 || reps < 0) {
             throw new Error("Hodnota musí být větší než 0!!");
         }
@@ -47,22 +52,43 @@ class StrengthTraining extends Workout {
         return this.duration * this.Intensity * this.sets * this.reps * 0.05;
     }
     getsummary(): string {
-        return `Posilování: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Série: ${this.sets}, Opakování: ${this.reps}, Spálené kalorie: ${this.calculateCaloriesBurned()} kcal.`;
+        return `Posilování: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Série: ${this.sets}, Opakování: ${this.reps}`;
     }
 }
 
 const myWorkouts: Workout[] = [];
+let itemId = 1;
 
 workoutCatalog.forEach(item => {
     if (item.type === 'running') {
-        myWorkouts.push(new Running(item.duration, item.Intensity, item.distance));
+        myWorkouts.push(new Running(itemId++, item.duration, item.Intensity, item.distance));
     } else if (item.type === 'strength') {
-        myWorkouts.push(new StrengthTraining(item.duration, item.Intensity, item.sets, item.reps));
+        myWorkouts.push(new StrengthTraining(itemId++, item.duration, item.Intensity, item.sets, item.reps));
     }
 });
 
-console.log("Souhrn tréninků:");
-myWorkouts.forEach(workout => {
-    console.log(workout.getsummary());
-    console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
-});
+// console.log("Souhrn tréninků:");
+// console.log(workout.getsummary());
+// console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
+export function renderResult():void {
+    var tbl = document.getElementById("myTable");
+    // ---
+    myWorkouts.forEach(workout => {
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        td1.appendChild(document.createTextNode(workout.getsummary()));
+        tr.appendChild(td1);
+        var td2 = document.createElement('td');
+        td2.setAttribute('class', 'w3-right-align');
+        td2.appendChild(document.createTextNode(workout.calculateCaloriesBurned().toString() + ' kcal'));
+        tr.appendChild(td2);
+        if (tbl) tbl.appendChild(tr);
+    });
+}
+// ---
+interface MyNamespacedWindow extends Window {
+    renderResult: object;
+}
+// ---
+declare var window: MyNamespacedWindow;
+window.renderResult=renderResult;
