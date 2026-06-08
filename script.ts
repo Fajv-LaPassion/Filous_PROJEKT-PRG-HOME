1905d0cbe0b9d07e9c606b02683988e4a044e0b0
Fajv-Lapassion <ondrej.filous@gmail.com>
2026-06-08 22:27:50 +0200

    přidání formuláře pro editaci aktivity Běh

--- script.ts
+++ script.ts
@@ -14,8 +14,21 @@ abstract class Workout {
     }
     abstract calculateCaloriesBurned():number;
     abstract getsummary():string;
-    public render():void {
-
+    public render(isNew: boolean):void {
+        var tbl = document.getElementById("myTable");
+        // ---
+        if (tbl && isNew) {
+            var tr = document.createElement('tr');
+            tr.id = "x"+this.id.toString();
+            var td1 = document.createElement('td');
+            td1.appendChild(document.createTextNode(this.getsummary()));
+            tr.appendChild(td1);
+            var td2 = document.createElement('td');
+            td2.setAttribute('class', 'w3-right-align');
+            td2.appendChild(document.createTextNode(this.calculateCaloriesBurned().toString() + ' kcal'));
+            tr.appendChild(td2);
+            tbl.appendChild(tr);
+        }
     }
 }
 
@@ -71,24 +84,77 @@ workoutCatalog.forEach(item => {
 // console.log(workout.getsummary());
 // console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
 export function renderResult():void {
-    var tbl = document.getElementById("myTable");
-    // ---
     myWorkouts.forEach(workout => {
-        var tr = document.createElement('tr');
-        var td1 = document.createElement('td');
-        td1.appendChild(document.createTextNode(workout.getsummary()));
-        tr.appendChild(td1);
-        var td2 = document.createElement('td');
-        td2.setAttribute('class', 'w3-right-align');
-        td2.appendChild(document.createTextNode(workout.calculateCaloriesBurned().toString() + ' kcal'));
-        tr.appendChild(td2);
-        if (tbl) tbl.appendChild(tr);
+        workout.render(true);
     });
 }
+
+export function openForm(id:number, frmId:string):void {
+    var frm = document.getElementById(frmId);
+    // ---
+    if (frm && frmId == "frmRunning") {
+        var inpId = document.getElementById("runningId");
+        var inp1 = document.getElementById("runningDuration");
+        var inp2 = document.getElementById("runningIntensity");
+        var inp3 = document.getElementById("runningDistance");
+        // ---
+        if (id == 0) {
+            inpId?.setAttribute("value", (itemId++).toString());
+            inp1?.setAttribute("value", "60");
+            inp2?.setAttribute("value", "1");
+            inp3?.setAttribute("value", "1");
+        }
+        else {
+            var item = myWorkouts.find(x => x.id == id) as Running;
+            if (item) {
+                inpId?.setAttribute("value", (id).toString());
+                inp1?.setAttribute("value", item.duration.toString());
+                inp2?.setAttribute("value", item.Intensity.toString());
+                inp3?.setAttribute("value", item.distance.toString());
+            } else return;
+        }
+        // ---
+        frm.style.display='block';
+        inp1?.focus();
+    }
+}
+
+export function saveItem(frmId: string):void {
+    var frm = document.getElementById(frmId);
+    // ---
+    if (frm && frmId == "frmRunning") {
+        var inpId = document.getElementById("runningId");
+        var inp1 = document.getElementById("runningDuration");
+        var inp2 = document.getElementById("runningIntensity");
+        var inp3 = document.getElementById("runningDistance");
+        // ---
+        var item = myWorkouts.find(x => x.id == Number(inpId?.getAttribute("value")));
+        if (item) {
+            // TODO: update hodnot a prekresleni radku (neni implementovano)
+            item.render(false);
+        }
+        else {
+            item = new Running(
+            Number(inpId?.getAttribute("value")), 
+            Number(inp1?.getAttribute("value")), 
+            Number(inp2?.getAttribute("value")), 
+            Number(inp3?.getAttribute("value")));
+            myWorkouts.push(item);
+            item.render(true);
+        }
+        // ---
+        frm.style.display='none'
+    }
+}
+
 // ---
 interface MyNamespacedWindow extends Window {
     renderResult: object;
+    openForm: object;
+    saveItem: object;
 }
 // ---
 declare var window: MyNamespacedWindow;
 window.renderResult=renderResult;
+window.openForm=openForm;
+window.saveItem=saveItem;
