0aec87c5980bb2c987f1729f63556d226d1e14d2
Fajv-Lapassion <ondrej.filous@gmail.com>
2026-06-09 20:44:23 +0200

    přidání druhého formuláře pro aktivitu posilování, výpočet celkového součtu spálených kalorií, výmaz zadaných aktivit, doplnění komentářů, aplikace dokončena

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ class Workout {
         this.duration = duration;
         this.Intensity = Intensity;
     }
+    // vykresleni nove pridaneho radku v tabulce s aktivitami
     render(isNew) {
         var tbl = document.getElementById("myTable");
         // ---
@@ -24,10 +25,24 @@ class Workout {
             td2.setAttribute('class', 'w3-right-align');
             td2.appendChild(document.createTextNode(this.calculateCaloriesBurned().toString() + ' kcal'));
             tr.appendChild(td2);
+            var td3 = document.createElement('td');
+            var btn1 = document.createElement('button');
+            btn1.setAttribute('class', 'w3-button w3-indigo w3-small w3-round w3-padding-small w3-center');
+            btn1.setAttribute('onclick', 'javascript:openForm(' + this.id.toString() + ', \'frm' + this.getTypeName() + '\');');
+            btn1.appendChild(document.createTextNode('edit'));
+            btn1.style = 'margin-right: 2px';
+            td3.appendChild(btn1);
+            var btn2 = document.createElement('button');
+            btn2.setAttribute('class', 'w3-button w3-red w3-small w3-round w3-padding-small w3-center');
+            btn2.setAttribute('onclick', 'javascript:delItem(' + this.id.toString() + ', this)');
+            btn2.appendChild(document.createTextNode('del'));
+            td3.appendChild(btn2);
+            tr.appendChild(td3);
             tbl.appendChild(tr);
         }
     }
 }
+// trida pro ulozeni aktivity typu beh
 class Running extends Workout {
     distance;
     constructor(id, duration, Intensity, distance) {
@@ -38,12 +53,14 @@ class Running extends Workout {
         this.distance = distance;
     }
     calculateCaloriesBurned() {
-        return this.duration * this.Intensity * this.distance * 0.1;
+        return this.duration * this.Intensity * this.distance * 1.2;
     }
     getsummary() {
         return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km`;
     }
+    getTypeName() { return "Running"; }
 }
+// trida pro ulozeni aktivity typu posilovani
 class StrengthTraining extends Workout {
     sets;
     reps;
@@ -56,14 +73,20 @@ class StrengthTraining extends Workout {
         this.reps = reps;
     }
     calculateCaloriesBurned() {
-        return this.duration * this.Intensity * this.sets * this.reps * 0.05;
+        return (this.duration * this.Intensity * 0.5) + (this.sets * this.reps * 0.1);
     }
     getsummary() {
         return `Posilování: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Série: ${this.sets}, Opakování: ${this.reps}`;
     }
+    getTypeName() { return "Strength"; }
 }
+// pole pro ulozeni seznamu objektu aktivit
 const myWorkouts = [];
+// cislovani radku v html tabulce, slouzi pro vyrobu ID radku
 let itemId = 1;
+// soucet spalenych kalorii ze vsech zadanych aktivit
+let kcalSUM = 0;
+// nacteni testovaciho datasetu aktivit po startu aplikace
 workoutCatalog.forEach(item => {
     if (item.type === 'running') {
         myWorkouts.push(new Running(itemId++, item.duration, item.Intensity, item.distance));
@@ -72,14 +95,25 @@ workoutCatalog.forEach(item => {
         myWorkouts.push(new StrengthTraining(itemId++, item.duration, item.Intensity, item.sets, item.reps));
     }
 });
-// console.log("Souhrn tréninků:");
-// console.log(workout.getsummary());
-// console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
+// prvotni vykresleni aktivit po spusteni aplikace
 export function renderResult() {
     myWorkouts.forEach(workout => {
         workout.render(true);
     });
+    // ---
+    recalculateSUM();
 }
+// prepocet celkoveho souctu spalenych kalorii a jeho zobrazeni
+function recalculateSUM() {
+    kcalSUM = 0;
+    myWorkouts.forEach(workout => {
+        kcalSUM += workout.calculateCaloriesBurned();
+    });
+    // ---
+    var cell = document.getElementById("kcalSUM");
+    cell.innerText = kcalSUM.toString() + ' kcal';
+}
+// otevreni formulare pro pridani/editaci aktivity
 export function openForm(id, frmId) {
     var frm = document.getElementById(frmId);
     // ---
@@ -90,18 +124,49 @@ export function openForm(id, frmId) {
         var inp3 = document.getElementById("runningDistance");
         // ---
         if (id == 0) {
-            inpId?.setAttribute("value", (itemId++).toString());
-            inp1?.setAttribute("value", "60");
-            inp2?.setAttribute("value", "1");
-            inp3?.setAttribute("value", "1");
+            inpId.value = (itemId++).toString();
+            inp1.valueAsNumber = 60;
+            inp2.valueAsNumber = 1;
+            inp3.valueAsNumber = 1;
         }
         else {
-            var item = myWorkouts.find(x => x.id == id);
+            let item = myWorkouts.find(x => x.id == id);
             if (item) {
-                inpId?.setAttribute("value", (id).toString());
-                inp1?.setAttribute("value", item.duration.toString());
-                inp2?.setAttribute("value", item.Intensity.toString());
-                inp3?.setAttribute("value", item.distance.toString());
+                inpId.value = id.toString();
+                inp1.valueAsNumber = item.duration;
+                inp2.valueAsNumber = item.Intensity;
+                inp3.valueAsNumber = item.distance;
+            }
+            else
+                return;
+        }
+        // ---
+        frm.style.display = 'block';
+        inp1?.focus();
+    }
+    // ---
+    if (frm && frmId == "frmStrength") {
+        var inpId = document.getElementById("strengthId");
+        var inp1 = document.getElementById("strengthDuration");
+        var inp2 = document.getElementById("strengthIntensity");
+        var inp3 = document.getElementById("strengthSets");
+        var inp4 = document.getElementById("strengthReps");
+        // ---
+        if (id == 0) {
+            inpId.value = (itemId++).toString();
+            inp1.valueAsNumber = 60;
+            inp2.valueAsNumber = 1;
+            inp3.valueAsNumber = 1;
+            inp4.valueAsNumber = 1;
+        }
+        else {
+            let item = myWorkouts.find(x => x.id == id);
+            if (item) {
+                inpId.value = id.toString();
+                inp1.valueAsNumber = item.duration;
+                inp2.valueAsNumber = item.Intensity;
+                inp3.valueAsNumber = item.sets;
+                inp4.valueAsNumber = item.reps;
             }
             else
                 return;
@@ -111,6 +176,7 @@ export function openForm(id, frmId) {
         inp1?.focus();
     }
 }
+// ulozeni nove aktivity ci aktualizace existujici
 export function saveItem(frmId) {
     var frm = document.getElementById(frmId);
     // ---
@@ -120,20 +186,87 @@ export function saveItem(frmId) {
         var inp2 = document.getElementById("runningIntensity");
         var inp3 = document.getElementById("runningDistance");
         // ---
-        var item = myWorkouts.find(x => x.id == Number(inpId?.getAttribute("value")));
+        let item = myWorkouts.find(x => x.id == Number(inpId?.value));
         if (item) {
-            // TODO: update hodnot a prekresleni radku (neni implementovano)
-            item.render(false);
+            item.duration = inp1.valueAsNumber;
+            item.Intensity = inp2.valueAsNumber;
+            item.distance = inp3.valueAsNumber;
+            // ---
+            var tr = document.getElementById('x' + item.id.toString());
+            if (tr) {
+                tr.cells[0].innerText = item.getsummary();
+                tr.cells[1].innerText = item.calculateCaloriesBurned().toString() + " kcal";
+            }
         }
         else {
-            item = new Running(Number(inpId?.getAttribute("value")), Number(inp1?.getAttribute("value")), Number(inp2?.getAttribute("value")), Number(inp3?.getAttribute("value")));
+            item = new Running(Number(inpId?.value), Number(inp1?.value), Number(inp2?.value), Number(inp3?.value));
             myWorkouts.push(item);
             item.render(true);
         }
         // ---
         frm.style.display = 'none';
     }
+    // ---
+    if (frm && frmId == "frmStrength") {
+        var inpId = document.getElementById("strengthId");
+        var inp1 = document.getElementById("strengthDuration");
+        var inp2 = document.getElementById("strengthIntensity");
+        var inp3 = document.getElementById("strengthSets");
+        var inp4 = document.getElementById("strengthReps");
+        // ---
+        let item = myWorkouts.find(x => x.id == Number(inpId?.value));
+        if (item) {
+            item.duration = inp1.valueAsNumber;
+            item.Intensity = inp2.valueAsNumber;
+            item.sets = inp3.valueAsNumber;
+            item.reps = inp4.valueAsNumber;
+            // ---
+            var tr = document.getElementById('x' + item.id.toString());
+            if (tr) {
+                tr.cells[0].innerText = item.getsummary();
+                tr.cells[1].innerText = item.calculateCaloriesBurned().toString() + " kcal";
+            }
+        }
+        else {
+            item = new StrengthTraining(Number(inpId?.value), Number(inp1?.value), Number(inp2?.value), Number(inp3?.value), Number(inp4?.value));
+            myWorkouts.push(item);
+            item.render(true);
+        }
+        // ---
+        frm.style.display = 'none';
+    }
+    // ---
+    recalculateSUM();
+}
+// vymaz zadane aktivity
+export function delItem(id, button) {
+    var index = myWorkouts.findIndex(x => x.id == id);
+    if (index != -1)
+        myWorkouts.splice(index, 1);
+    // ---
+    var row = button.closest('tr');
+    if (row)
+        row.remove();
+    // ---
+    recalculateSUM();
+}
+// vymaz vsech aktivit, tj. vycisteni tabulky
+export function delAllItems() {
+    myWorkouts.length = 0;
+    // ---
+    var tbl = document.getElementById("myTable");
+    if (tbl) {
+        // Loop backwards because table.rows is a live array that shrinks as you delete
+        // Keep header and footer intact
+        for (let i = tbl.rows.length - 2; i >= 1; i--) {
+            tbl.deleteRow(i);
+        }
+    }
+    // ---
+    recalculateSUM();
 }
 window.renderResult = renderResult;
 window.openForm = openForm;
 window.saveItem = saveItem;
+window.delItem = delItem;
+window.delAllItems = delAllItems;
--- script.ts
+++ script.ts
@@ -12,8 +12,13 @@ abstract class Workout {
         this.duration = duration;
         this.Intensity = Intensity;
     }
+    // spocte a vrati pocet spalenych kalorii
     abstract calculateCaloriesBurned():number;
+    // vrati aktivitu v textove podobe
     abstract getsummary():string;
+    // vrati nazev tridy
+    abstract getTypeName():string;
+    // vykresleni nove pridaneho radku v tabulce s aktivitami
     public render(isNew: boolean):void {
         var tbl = document.getElementById("myTable");
         // ---
@@ -27,11 +32,25 @@ abstract class Workout {
             td2.setAttribute('class', 'w3-right-align');
             td2.appendChild(document.createTextNode(this.calculateCaloriesBurned().toString() + ' kcal'));
             tr.appendChild(td2);
+            var td3 = document.createElement('td');
+            var btn1 = document.createElement('button');
+            btn1.setAttribute('class', 'w3-button w3-indigo w3-small w3-round w3-padding-small w3-center');
+            btn1.setAttribute('onclick', 'javascript:openForm('+this.id.toString()+', \'frm'+this.getTypeName()+'\');');
+            btn1.appendChild(document.createTextNode('edit'));
+            btn1.style = 'margin-right: 2px';
+            td3.appendChild(btn1);
+            var btn2 = document.createElement('button');
+            btn2.setAttribute('class', 'w3-button w3-red w3-small w3-round w3-padding-small w3-center');
+            btn2.setAttribute('onclick', 'javascript:delItem('+this.id.toString()+', this)');
+            btn2.appendChild(document.createTextNode('del'));
+            td3.appendChild(btn2);
+            tr.appendChild(td3);
             tbl.appendChild(tr);
         }
     }
 }
 
+// trida pro ulozeni aktivity typu beh
 class Running extends Workout {
     distance:number;
     constructor(id:number, duration:number, Intensity:number, distance:number) {
@@ -42,17 +61,18 @@ class Running extends Workout {
         this.distance = distance;
     }
     calculateCaloriesBurned(): number {
-        return this.duration * this.Intensity * this.distance * 0.1;
+        return this.duration * this.Intensity * this.distance * 1.2;
     }
     getsummary(): string {
         return `Běh: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Vzdálenost: ${this.distance} km`;
     }
+    getTypeName(): string { return "Running"; }
 }
 
+// trida pro ulozeni aktivity typu posilovani
 class StrengthTraining extends Workout {
     sets:number;
     reps:number;
-
     constructor(id:number, duration:number, Intensity:number, sets:number, reps:number) {
         super(id, duration, Intensity);
         if (sets < 0 || reps < 0) {
@@ -62,16 +82,22 @@ class StrengthTraining extends Workout {
         this.reps = reps;
     }
     calculateCaloriesBurned(): number {
-        return this.duration * this.Intensity * this.sets * this.reps * 0.05;
+        return (this.duration * this.Intensity * 0.5) + (this.sets * this.reps * 0.1);
     }
     getsummary(): string {
         return `Posilování: Doba trvání: ${this.duration} minut, Intenzita: ${this.Intensity}, Série: ${this.sets}, Opakování: ${this.reps}`;
     }
+    getTypeName(): string { return "Strength"; }
 }
 
+// pole pro ulozeni seznamu objektu aktivit
 const myWorkouts: Workout[] = [];
+// cislovani radku v html tabulce, slouzi pro vyrobu ID radku
 let itemId = 1;
+// soucet spalenych kalorii ze vsech zadanych aktivit
+let kcalSUM = 0;
 
+// nacteni testovaciho datasetu aktivit po startu aplikace
 workoutCatalog.forEach(item => {
     if (item.type === 'running') {
         myWorkouts.push(new Running(itemId++, item.duration, item.Intensity, item.distance));
@@ -80,37 +106,78 @@ workoutCatalog.forEach(item => {
     }
 });
 
-// console.log("Souhrn tréninků:");
-// console.log(workout.getsummary());
-// console.log('Spálené kalorie: ' + workout.calculateCaloriesBurned() + ' kcal');
+// prvotni vykresleni aktivit po spusteni aplikace
 export function renderResult():void {
     myWorkouts.forEach(workout => {
         workout.render(true);
     });
+    // ---
+    recalculateSUM();
 }
 
+// prepocet celkoveho souctu spalenych kalorii a jeho zobrazeni
+function recalculateSUM():void {
+    kcalSUM = 0;
+    myWorkouts.forEach(workout => {
+        kcalSUM += workout.calculateCaloriesBurned();
+    });
+    // ---
+    var cell = document.getElementById("kcalSUM") as HTMLTableCellElement;
+    cell.innerText = kcalSUM.toString() + ' kcal';
+}
+
+// otevreni formulare pro pridani/editaci aktivity
 export function openForm(id:number, frmId:string):void {
     var frm = document.getElementById(frmId);
     // ---
     if (frm && frmId == "frmRunning") {
-        var inpId = document.getElementById("runningId");
-        var inp1 = document.getElementById("runningDuration");
-        var inp2 = document.getElementById("runningIntensity");
-        var inp3 = document.getElementById("runningDistance");
+        var inpId = document.getElementById("runningId") as HTMLInputElement;
+        var inp1 = document.getElementById("runningDuration") as HTMLInputElement;
+        var inp2 = document.getElementById("runningIntensity") as HTMLInputElement;
+        var inp3 = document.getElementById("runningDistance") as HTMLInputElement;
         // ---
         if (id == 0) {
-            inpId?.setAttribute("value", (itemId++).toString());
-            inp1?.setAttribute("value", "60");
-            inp2?.setAttribute("value", "1");
-            inp3?.setAttribute("value", "1");
+            inpId.value = (itemId++).toString();
+            inp1.valueAsNumber = 60;
+            inp2.valueAsNumber = 1;
+            inp3.valueAsNumber = 1;
         }
         else {
-            var item = myWorkouts.find(x => x.id == id) as Running;
+            let item = myWorkouts.find(x => x.id == id) as Running;
             if (item) {
-                inpId?.setAttribute("value", (id).toString());
-                inp1?.setAttribute("value", item.duration.toString());
-                inp2?.setAttribute("value", item.Intensity.toString());
-                inp3?.setAttribute("value", item.distance.toString());
+                inpId.value = id.toString();
+                inp1.valueAsNumber = item.duration;
+                inp2.valueAsNumber = item.Intensity;
+                inp3.valueAsNumber = item.distance;
+            } else return;
+        }
+        // ---
+        frm.style.display='block';
+        inp1?.focus();
+    }
+    // ---
+    if (frm && frmId == "frmStrength") {
+        var inpId = document.getElementById("strengthId") as HTMLInputElement;
+        var inp1 = document.getElementById("strengthDuration") as HTMLInputElement;
+        var inp2 = document.getElementById("strengthIntensity") as HTMLInputElement;
+        var inp3 = document.getElementById("strengthSets") as HTMLInputElement;
+        var inp4 = document.getElementById("strengthReps") as HTMLInputElement;
+        // ---
+        if (id == 0) {
+            inpId.value = (itemId++).toString();
+            inp1.valueAsNumber = 60;
+            inp2.valueAsNumber = 1;
+            inp3.valueAsNumber = 1;
+            inp4.valueAsNumber = 1;
+        }
+        else {
+            let item = myWorkouts.find(x => x.id == id) as StrengthTraining;
+            if (item) {
+                inpId.value = id.toString();
+                inp1.valueAsNumber = item.duration;
+                inp2.valueAsNumber = item.Intensity;
+                inp3.valueAsNumber = item.sets;
+                inp4.valueAsNumber = item.reps;
             } else return;
         }
         // ---
@@ -119,42 +186,108 @@ export function openForm(id:number, frmId:string):void {
     }
 }
 
+// ulozeni nove aktivity ci aktualizace existujici
 export function saveItem(frmId: string):void {
     var frm = document.getElementById(frmId);
     // ---
     if (frm && frmId == "frmRunning") {
-        var inpId = document.getElementById("runningId");
-        var inp1 = document.getElementById("runningDuration");
-        var inp2 = document.getElementById("runningIntensity");
-        var inp3 = document.getElementById("runningDistance");
+        var inpId = document.getElementById("runningId") as HTMLInputElement;
+        var inp1 = document.getElementById("runningDuration") as HTMLInputElement;
+        var inp2 = document.getElementById("runningIntensity") as HTMLInputElement;
+        var inp3 = document.getElementById("runningDistance") as HTMLInputElement;
         // ---
-        var item = myWorkouts.find(x => x.id == Number(inpId?.getAttribute("value")));
+        let item = myWorkouts.find(x => x.id == Number(inpId?.value)) as Running;
         if (item) {
-            // TODO: update hodnot a prekresleni radku (neni implementovano)
-            item.render(false);
+            item.duration = inp1.valueAsNumber;
+            item.Intensity = inp2.valueAsNumber;
+            item.distance = inp3.valueAsNumber;
+            // ---
+            var tr = document.getElementById('x'+item.id.toString()) as HTMLTableRowElement;
+            if (tr) {
+                tr.cells[0].innerText = item.getsummary();
+                tr.cells[1].innerText = item.calculateCaloriesBurned().toString() + " kcal";
+            }
         }
         else {
-            item = new Running(
-            Number(inpId?.getAttribute("value")), 
-            Number(inp1?.getAttribute("value")), 
-            Number(inp2?.getAttribute("value")), 
-            Number(inp3?.getAttribute("value")));
+            item = new Running(Number(inpId?.value), Number(inp1?.value), Number(inp2?.value), Number(inp3?.value));
             myWorkouts.push(item);
             item.render(true);
         }
         // ---
         frm.style.display='none'
     }
+    // ---
+    if (frm && frmId == "frmStrength") {
+        var inpId = document.getElementById("strengthId") as HTMLInputElement;
+        var inp1 = document.getElementById("strengthDuration") as HTMLInputElement;
+        var inp2 = document.getElementById("strengthIntensity") as HTMLInputElement;
+        var inp3 = document.getElementById("strengthSets") as HTMLInputElement;
+        var inp4 = document.getElementById("strengthReps") as HTMLInputElement;
+        // ---
+        let item = myWorkouts.find(x => x.id == Number(inpId?.value)) as StrengthTraining;
+        if (item) {
+            item.duration = inp1.valueAsNumber;
+            item.Intensity = inp2.valueAsNumber;
+            item.sets = inp3.valueAsNumber;
+            item.reps = inp4.valueAsNumber;
+            // ---
+            var tr = document.getElementById('x'+item.id.toString()) as HTMLTableRowElement;
+            if (tr) {
+                tr.cells[0].innerText = item.getsummary();
+                tr.cells[1].innerText = item.calculateCaloriesBurned().toString() + " kcal";
+            }
+        }
+        else {
+            item = new StrengthTraining(Number(inpId?.value), Number(inp1?.value), Number(inp2?.value), Number(inp3?.value), Number(inp4?.value));
+            myWorkouts.push(item);
+            item.render(true);
+        }
+        // ---
+        frm.style.display='none'
+    }
+    // ---
+    recalculateSUM();
 }
 
-// ---
+// vymaz zadane aktivity
+export function delItem(id: number, button: HTMLButtonElement):void {   
+    var index = myWorkouts.findIndex(x => x.id == id);
+    if (index != -1) myWorkouts.splice(index, 1);
+    // ---
+    var row = button.closest('tr') as HTMLTableRowElement;
+    if (row) row.remove();
+    // ---
+    recalculateSUM();
+}
+
+// vymaz vsech aktivit, tj. vycisteni tabulky
+export function delAllItems():void {
+    myWorkouts.length = 0;
+    // ---
+    var tbl = document.getElementById("myTable") as HTMLTableElement;
+    if (tbl) {
+        // Loop backwards because table.rows is a live array that shrinks as you delete
+        // Keep header and footer intact
+        for (let i = tbl.rows.length - 2; i >= 1; i--) {
+            tbl.deleteRow(i); 
+        }
+    }
+    // ---
+    recalculateSUM();
+}
+
+// interface pro zpristupneni vybranych funkci v index.html
 interface MyNamespacedWindow extends Window {
     renderResult: object;
     openForm: object;
     saveItem: object;
+    delItem: object;
+    delAllItems: object;
 }
 // ---
 declare var window: MyNamespacedWindow;
 window.renderResult=renderResult;
 window.openForm=openForm;
 window.saveItem=saveItem;
+window.delItem=delItem;
+window.delAllItems=delAllItems;
